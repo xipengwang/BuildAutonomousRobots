365efec1abe99a9e06ff20dc28fff398ca67e342
Xipeng Wang <xipengw@umich.edu>
2021-03-03 22:18:51 -0500

    WIP on odometry.

--- motor-remote-control/motor-remote-control.ino
+++ motor-remote-control/motor-remote-control.ino
@@ -41,12 +41,13 @@ enum RobotBehavior { kRobotBehaviorGo, kRobotBehaviorDodge };
 
 SemaphoreHandle_t policy_signal;
 SemaphoreHandle_t wheel_speed_signal;
+SemaphoreHandle_t yaw_signal;
 
 volatile enum RobotBehavior behavior_policy = kRobotBehaviorGo;
 
-double target_speed = 0.5; //[m/s]
+double left_target_speed = 0.5;  //[m/s]
+double right_target_speed = 0.5; //[m/s]
 
-volatile double wheel_speed = 0.0;
 volatile double left_wheel_speed = 0.0;
 volatile double right_wheel_speed = 0.0;
 volatile unsigned long echo_initial_time = 0;
@@ -55,7 +56,9 @@ volatile unsigned long right_wheel_encoder_cnt = 0;
 volatile unsigned long left_wheel_encoder_cnt = 0;
 BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-float yaw_bias = 0.0;
+double yaw_bias = 0.0;
+double yaw = 0;
+double pos_x = 0, pos_y = 0;
 
 void MotorControl(void *pvParameters);
 void CollisionAvoidance(void *pvParameters);
@@ -90,6 +93,7 @@ void setup() {
 
   policy_signal = xSemaphoreCreateBinary();
   wheel_speed_signal = xSemaphoreCreateBinary();
+  yaw_signal = xSemaphoreCreateBinary();
 
   ITimer1.init();
   if (ITimer1.attachInterruptInterval(kTimer1IntervalMs, HandleTimer)) {
@@ -167,17 +171,32 @@ void MotorControl(void *pvParameters) {
   // initialize digital pin 13 as an output.
   // pinMode(LED_BUILTIN, OUTPUT);
 
-  double local_wheel_speed = wheel_speed;
+  double local_right_wheel_speed = right_wheel_speed;
+  double local_left_wheel_speed = left_wheel_speed;
+  double local_yaw = yaw;
   enum RobotBehavior local_behavior_policy = behavior_policy;
 
   // Specify the links and initial tuning parameters
   double Kp = 2.0, Ki = 1.0, Kd = 0;
-  double output_pwm;
-  PID speed_pid(&wheel_speed, &output_pwm, &target_speed, Kp, Ki, Kd, DIRECT);
-  speed_pid.SetMode(AUTOMATIC);
-  speed_pid.SetSampleTime(kMotorControlIntervalMs);
-  speed_pid.SetOutputLimits(0, 1);
+  double l_output_pwm;
+  double r_output_pwm;
+  PID l_speed_pid(&local_left_wheel_speed, &l_output_pwm, &left_target_speed,
+                  Kp, Ki, Kd, DIRECT);
+  PID r_speed_pid(&local_right_wheel_speed, &r_output_pwm, &right_target_speed,
+                  Kp, Ki, Kd, DIRECT);
+  l_speed_pid.SetMode(AUTOMATIC);
+  l_speed_pid.SetSampleTime(kMotorControlIntervalMs);
+  l_speed_pid.SetOutputLimits(0, 1);
+  r_speed_pid.SetMode(AUTOMATIC);
+  r_speed_pid.SetSampleTime(kMotorControlIntervalMs);
+  r_speed_pid.SetOutputLimits(0, 1);
+
   unsigned long current_time = 0, previous_time = 0;
+  double previous_speed =
+      (local_left_wheel_speed + local_right_wheel_speed) / 2;
+  double previous_yaw = local_yaw;
+  double current_speed = previous_speed;
+  double current_yaw = previous_yaw;
   for (;;) // A Task shall never return or exit.
   {
     /* Inspect our own high water mark on entering the task. */
@@ -192,26 +211,73 @@ void MotorControl(void *pvParameters) {
     if (current_time - previous_time < kMotorControlIntervalMs) {
       continue;
     }
-    previous_time = current_time;
     if (xSemaphoreTake(policy_signal, (TickType_t)10) == pdTRUE) {
       local_behavior_policy = behavior_policy;
     }
 
     if (xSemaphoreTake(wheel_speed_signal, (TickType_t)10) == pdTRUE) {
-      local_wheel_speed = wheel_speed;
+      local_right_wheel_speed = right_wheel_speed;
+      local_left_wheel_speed = left_wheel_speed;
     }
-    if (abs(local_wheel_speed - target_speed) < 0.05) {
-      local_wheel_speed = target_speed;
+
+    if (xSemaphoreTake(yaw_signal, (TickType_t)10) == pdTRUE) {
+      local_yaw = yaw;
     }
-    speed_pid.Compute();
+
+    current_speed = (local_left_wheel_speed + local_right_wheel_speed) / 2;
+    current_yaw = local_yaw;
+    double move_dist = (current_speed + previous_speed) *
+                       (current_time - previous_time) / 2000;
+    double heading_change = current_yaw - previous_yaw;
+    if (heading_change > 90) {
+      heading_change -= 360;
+    } else if (heading_change < -90) {
+      heading_change += 360.0;
+    }
+    double heading_change_rad = heading_change * 3.1415926 / 180.0;
+    double delta_x = move_dist;
+    double delta_y = 0;
+    if (heading_change != 0) {
+      double R = move_dist / heading_change_rad;
+      delta_x = R * sin(heading_change_rad);
+      delta_y = R * (1 - cos(heading_change_rad));
+    }
+    if (heading_change_rad < 0) {
+      delta_y *= -1;
+    }
+    double c = cos(previous_yaw * 3.1415926 / 180.0);
+    double s = sin(previous_yaw * 3.1415926 / 180.0);
+    pos_x += c * delta_x - s * delta_y;
+    pos_y += s * delta_x + c * delta_y;
+
+    Serial.print("y:");
+    Serial.print(pos_y);
+    Serial.print(",");
+    Serial.print("x:");
+    Serial.println(pos_x);
+    previous_speed = current_speed;
+    previous_yaw = current_yaw;
+    previous_time = current_time;
+
+    if (abs(local_left_wheel_speed - left_target_speed) < 0.05) {
+      local_left_wheel_speed = left_target_speed;
+    }
+    if (abs(local_right_wheel_speed - right_target_speed) < 0.05) {
+      local_right_wheel_speed = right_target_speed;
+    }
+
     if (local_behavior_policy == kRobotBehaviorGo) {
-      LeftMotorControl(kMotorDirectionForward, output_pwm * motor_pwm);
-      RightMotorControl(kMotorDirectionForward, output_pwm * motor_pwm);
+      left_target_speed = 0.5;
+      right_target_speed = 0.5;
     } else if (local_behavior_policy == kRobotBehaviorDodge) {
-      LeftMotorControl(kMotorDirectionStop, 0);
-      RightMotorControl(kMotorDirectionBackward, output_pwm * motor_pwm);
+      left_target_speed = 0.3;
+      right_target_speed = 0;
       // digitalWrite(LED_BUILTIN, LOW);
     }
+    l_speed_pid.Compute();
+    r_speed_pid.Compute();
+    LeftMotorControl(kMotorDirectionForward, l_output_pwm * motor_pwm);
+    RightMotorControl(kMotorDirectionForward, r_output_pwm * motor_pwm);
   }
 }
 
@@ -262,7 +328,7 @@ ISR(PCINT0_vect) { HandleEcho(); } // end of PCINT0_vect
 void HandleTimer() {
   xHigherPriorityTaskWoken = pdFALSE;
   left_wheel_speed = 0.7 * left_wheel_speed + 0.3 * kMeterPerResolution *
-                                                  right_wheel_encoder_cnt *
+                                                  left_wheel_encoder_cnt *
                                                   1000 / kTimer1IntervalMs;
   right_wheel_speed = 0.7 * right_wheel_speed + 0.3 * kMeterPerResolution *
                                                     right_wheel_encoder_cnt *
@@ -270,7 +336,6 @@ void HandleTimer() {
   right_wheel_encoder_cnt = 0;
   left_wheel_encoder_cnt = 0;
   // Hack
-  wheel_speed = right_wheel_speed;
   xSemaphoreGiveFromISR(wheel_speed_signal, &xHigherPriorityTaskWoken);
   if (xHigherPriorityTaskWoken == pdTRUE) {
     portYIELD_FROM_ISR();
@@ -313,7 +378,6 @@ void HandleIMU(void * /*pvParameters*/) {
   float previous_gyro_z = 0.0;
   float gyro_z = 0.0;
   bool first = true;
-  float yaw = 0;
   unsigned long current_time, previous_time;
   for (;;) // A Task shall never return or exit.
   {
@@ -329,14 +393,21 @@ void HandleIMU(void * /*pvParameters*/) {
       first = false;
       continue;
     }
-    Serial.print("gyro_z:");
-    Serial.print(gyro_z);
-    Serial.print(",");
-    Serial.print("yaw:");
-    Serial.println(yaw);
+    // Serial.print("gyro_z:");
+    // Serial.print(gyro_z * 3.1415926 / 180.0);
+    // Serial.print(",");
+    // Serial.print("x:");
+    // Serial.print(pos_x);
+    // Serial.print(",");
+    // Serial.print("y:");
+    // Serial.print(pos_y);
+    // Serial.print(",");
+    // Serial.print("yaw:");
+    // Serial.println(yaw * 3.1415926 / 180.0);
     current_time = millis();
     yaw = WrapAngle(yaw + (previous_gyro_z + gyro_z) *
                               (current_time - previous_time) / 2000);
+    xSemaphoreGive(yaw_signal);
     previous_gyro_z = gyro_z;
     previous_time = current_time;
   }
@@ -351,7 +422,7 @@ void StartIMU() {
   Wire.endTransmission(true); // end the transmission
 }
 
-float QueryGyroZ() {
+double QueryGyroZ() {
   Wire.beginTransmission(MPU6050);
   Wire.write(0x47);
   Wire.endTransmission(false);
